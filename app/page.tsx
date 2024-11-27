45abc5e9e29497d757474f127609d1f5031b3f7e
Lortas102066 <mitsukimorinaga@gmail.com>
2024-11-28 10:13:19 +1100

    New theme color

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,31 @@
-'use client'
-
 import Image from 'next/image'
-import { useRouter } from "next/navigation"
-
 export default function Component() {
-  const router = useRouter();
-  const handleClick = () => {
-    router.push("https://docs.google.com/forms/d/e/1FAIpQLSfdOD8TSZpCkxhMFBfBoxLAAulzIQry6R7dNF0PTWgf_m-sFA/viewform?fbclid=PAZXh0bgNhZW0CMTEAAaapUSdXg3q7NG9Muxvdge-31IuAuMX-NKCwmhp5bEG13hwED32AuwnQif8_aem_CwvYxZM0qVmXutCqjwgO_g");
-  };
+  const link = "https://docs.google.com/forms/d/e/1FAIpQLSfdOD8TSZpCkxhMFBfBoxLAAulzIQry6R7dNF0PTWgf_m-sFA/viewform?fbclid=PAZXh0bgNhZW0CMTEAAaapUSdXg3q7NG9Muxvdge-31IuAuMX-NKCwmhp5bEG13hwED32AuwnQif8_aem_CwvYxZM0qVmXutCqjwgO_g";
 
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-[#282828] px-4 py-12">
+    <div className="min-h-screen flex flex-col items-center justify-center bg-[#000000] px-4 py-12">
       <div className="relative w-full max-w-4xl mx-auto">
         <div className="absolute inset-0 flex flex-col items-center justify-center z-10 space-y-4 p-4">
-          <p className="text-3xl sm:text-5xl md:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 text-center">
-            Diamond Breaker Club
-          </p>
-          <p className="text-xl md:text-2xl text-gray-300 text-center">
-            La Trobe University
-          </p>
-          <button 
-            className="shadow-[0_4px_14px_0_rgb(0,0,0,10%)] hover:shadow-[0_6px_20px_rgba(93,93,93,23%)] hover:bg-white hover:text-black px-8 py-2 bg-[#fff] text-[#696969] rounded-md font-bold transition duration-200 ease-linear text-lg sm:text-xl md:text-2xl hover:scale-105"
-            onClick={handleClick}
-          >
-            Register Now
-          </button>
+          <div>
+            <span className="absolute mx-auto py-4 flex border w-fit bg-gradient-to-r blur-xl from-blue-500 via-teal-500 to-pink-500 bg-clip-text text-6xl box-content font-extrabold text-transparent text-center select-none">
+              Diamond Breaker Club
+            </span>
+            <h1
+                className="relative top-0 w-fit h-auto py-4 justify-center flex bg-gradient-to-r items-center from-blue-500 via-teal-500 to-pink-500 bg-clip-text text-6xl font-extrabold text-transparent text-center select-auto">
+                Diamond Breaker Club
+            </h1>
+          </div>
+          <div>
+          <div className="relative inline-flex  group">
+              <div
+                  className="absolute transitiona-all duration-1000 opacity-70 -inset-px bg-gradient-to-r from-[#44BCFF] via-[#FF44EC] to-[#FF675E] rounded-xl blur-lg group-hover:opacity-100 group-hover:-inset-1 group-hover:duration-200 animate-tilt">
+              </div>
+              <a href={link} title="Register Now"
+                  className="relative inline-flex items-center justify-center px-8 py-4 text-lg font-bold text-white transition-all duration-200 bg-gray-900 font-pj rounded-xl focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900"
+                  role="button">Register Now
+              </a>
+          </div>
+          </div>
         </div>
         
         <div className="relative w-full h-0 pb-[64%]">
